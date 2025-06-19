b48c99f387a2db7f79dd0a593872be5d68fdd116
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-06-19 08:28:07 +0700

    Create create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,43 @@
+-- Tabel Jurusan
+CREATE TABLE jurusan (
+    kode_jur VARCHAR(2) PRIMARY KEY NOT NULL,
+    nama_jur VARCHAR(50) NOT NULL,
+    jenjang VARCHAR(10) NOT NULL
+);
+
+-- Tabel Mahasiswa
+CREATE TABLE mahasiswa (
+    nim VARCHAR(10) PRIMARY KEY NOT NULL,
+    nama_m VARCHAR(50) NOT NULL,
+    tpt_lhr_m VARCHAR(20) NOT NULL,
+    tgl_lhr_m DATE NOT NULL,
+    j_kelamin VARCHAR(10) NOT NULL,
+    alm_m VARCHAR(100) NOT NULL,
+    kota_m VARCHAR(20) NOT NULL,
+    agama_m VARCHAR(10) NOT NULL,
+    telepon_m VARCHAR(20) NOT NULL,
+    kode_jur VARCHAR(2) NOT NULL,
+    FOREIGN KEY (kode_jur) REFERENCES jurusan(kode_jur)
+);
+
+-- Tabel Matakuliah
+CREATE TABLE matakuliah (
+    kdmk VARCHAR(10) PRIMARY KEY NOT NULL,
+    nama_mk VARCHAR(50) NOT NULL,
+    sks INT NOT NULL,
+    semester VARCHAR(2) NOT NULL
+);
+
+-- Tabel Nilai
+CREATE TABLE nilai (
+    nim VARCHAR(10) NOT NULL,
+    kdmk VARCHAR(10) NOT NULL,
+    smt VARCHAR(2) NOT NULL,
+    nilai_absen INT NOT NULL,
+    nilai_tugas INT NOT NULL,
+    nilai_uts INT NOT NULL,
+    nilai_uas INT NOT NULL,
+    PRIMARY KEY (nim, kdmk),
+    FOREIGN KEY (nim) REFERENCES mahasiswa(nim),
+    FOREIGN KEY (kdmk) REFERENCES matakuliah(kdmk)
+);
